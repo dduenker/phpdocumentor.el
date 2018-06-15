d0b50ff350ea1717cfbf4f901f0621a73974c00b
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-06-16 00:41:55 +0900

    phpdoc-insert-new-line

--- phpdocumentor.el
+++ phpdocumentor.el
@@ -37,10 +37,10 @@
   (setq inicio (point))
   (setq init-block-point (point))
   (phpdoc-insert-doc-start)     ; insert /**
-  ;; (phpdoc-new-line method-name) ; insert  * method-name
-  ;; (phpdoc-new-line)             ; insert  *
+  ;; (phpdoc-insert-new-line method-name) ; insert  * method-name
+  ;; (phpdoc-insert-new-line)             ; insert  *
   (phpdoc-insert-params params) ; insert  * @param param
-  (phpdoc-new-line "@return ")  ; insert  * @return
+  (phpdoc-insert-new-line "@return ")  ; insert  * @return
   (phpdoc-insert-doc-end)       ; insert  */
   (indent-region inicio (point))
   (goto-char init-block-point)
@@ -52,14 +52,14 @@
   "create-the-setter-for-a-variable"
   (interactive)
   (search-backward "$")
-  (setq method-name (phpdoc-get-method-description))
-  (setq params (phpdoc-get-params))
-  (phpdoc-block-position)
+  ;; (setq method-name (phpdoc-get-method-description))
+  ;; (setq params (phpdoc-get-params))
+  ;; (phpdoc-block-position)
   (setq inicio (point))
   (setq init-block-point (point))
   (phpdoc-insert-doc-start)
-  (phpdoc-new-line method-name)
-  (phpdoc-new-line)
+  (phpdoc-insert-new-line method-name)
+  (phpdoc-insert-new-line)
   (phpdoc-insert-params params)
   (phpdoc-insert-doc-end)
   (indent-region inicio (point))
@@ -74,7 +74,7 @@
   (newline)
 )
 
-(defun phpdoc-new-line (&optional phpdoc-str)
+(defun phpdoc-insert-new-line (&optional phpdoc-str)
   (insert (concat "* " phpdoc-str))
   (newline)
 )
@@ -102,7 +102,7 @@
 (defun phpdoc-insert-params (param-list)
   (if (> (length param-list) 0)
   (while param-list
-    (phpdoc-new-line (concat "@param " (string-trim (car param-list))))
+    (phpdoc-insert-new-line (concat "@param " (string-trim (car param-list))))
     (setq param-list (cdr param-list))
    )
   )
@@ -116,6 +116,3 @@
   (replace-regexp-in-string ")" "" (replace-regexp-in-string "$+" "" params))
   (setq param-list (split-string (replace-regexp-in-string ")" "" params) ","))
 )
-
-;;(fset 'php-create-setter
-;;   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217848 115 97 backspace 101 97 114 tab 98 97 tab return 36 return right 67108896 C-right 134217847 134217790 134217848 115 101 97 114 tab 98 97 tab return 125 return return up return tab 112 117 98 99 108 105 backspace backspace backspace 108 105 99 32 102 117 110 99 105 backspace backspace 99 116 105 111 110 32 115 101 116 25 C-left 21 51 right 67108896 right 134217848 99 97 112 105 116 97 108 105 tab 45 114 101 103 tab return 5 40 36 25 41 32 123 return tab 36 116 104 105 115 45 62 25 32 61 32 36 25 59 return 125 return] 0 "%d")) arg)))
